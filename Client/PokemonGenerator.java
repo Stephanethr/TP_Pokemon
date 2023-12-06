e751e91fa2041dafb950eeea35131f03cfde87cb
Stephanethr <stethiry9@gmail.com>
2023-12-06 11:20:16 +0100

    create PokemonGenerator class

---
+++ Client/PokemonGenerator.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class PokemonGenerator {
+    private static final Random random = new Random();
+    private static final List<String> infosPokemons = loadPokemonInfo();
+
+    private static List<String> loadPokemonInfo() {
+        List<String> pokemonInfoList = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("../pokemonsNonEvolue.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                pokemonInfoList.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return pokemonInfoList;
+    }
+
+    public static Pokemon generateRandomPokemon() {
+        // Choisir une ligne aléatoire dans la liste des infosPokemons
+        String randomPokemonInfo = infosPokemons.get(random.nextInt(infosPokemons.size()));
+
+        // Diviser la ligne en nom et type en utilisant la virgule comme séparateur
+        String[] parts = randomPokemonInfo.split(",");
+        String nom = parts[0];
+        String type = parts[1].trim(); // Trim pour enlever les espaces éventuels
+
+        // Les autres attributs du Pokémon
+        int pc = random.nextInt(100) + 50;
+        int pv = random.nextInt(100) + 50;
+
+        return new Pokemon(nom, type, pc, pv);
+    }
+}
