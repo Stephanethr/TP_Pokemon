d43b8ec4326f1a4e1dc28f0a94b2810ecef73f0d
Stephanethr <stethiry9@gmail.com>
2023-12-06 09:52:05 +0100

    init serveur

---
+++ Serveur/AcceptClient.java
@@ -0,0 +1,34 @@
+package TP_Pokemon.Serveur;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class AcceptClient extends Thread {
+    private ServerSocket server;
+    private Socket socket;
+    private PrintWriter out;
+
+    public AcceptClient(ServerSocket server) {
+        this.server = server;
+    }
+
+    public void run() {
+        System.out.println("Serveur en attente de connexion sur le port 2000");
+        int nbClients = 0;
+        try {
+            while (true) {
+                nbClients++;
+                socket = server.accept();
+                System.out.println("Utilisateur connecté #" + nbClients);
+                out = new PrintWriter(socket.getOutputStream());
+                out.println("Bienvenue sur PokeJava !");
+                out.flush();
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Serveur/Serveur.java
@@ -0,0 +1,15 @@
+package TP_Pokemon.Serveur;
+
+import java.net.ServerSocket;
+public class Serveur {
+    public static void main(String[] args) {
+        ServerSocket server;
+        try{
+            server = new ServerSocket(2000);
+            AcceptClient client = new AcceptClient(server);
+            client.start();
+        }catch(Exception e){
+            System.out.println("Erreur : " + e);
+        }
+    }
+}
