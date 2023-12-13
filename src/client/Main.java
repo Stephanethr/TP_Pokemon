b28102bb64823fd97729917d14e7280535025e87
Frédéric Turcq-Santucci <fturcq.santucci@gmail.com>
2023-12-13 10:28:19 +0100

    sauvegarde et charger

--- src/client/Main.java
+++ src/client/Main.java
@@ -1,17 +1,79 @@
 package src.client;
-
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
 
         public static void main(String[] args) {
 
+        String filePath = "";
+        String pseudo = "";
+        Dresseur dresseur = null;
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Bienvenue dans le monde des Pokémons ! Quel est votre pseudo ?");
+        System.out.println("Bienvenue dans le monde des Pokémons !");
 
-        String pseudo = sc.nextLine();
-        Dresseur dresseur = new Dresseur(pseudo);
+        
+        System.out.println("Avez vous un compte ? (y/n)");
+        String reponse = sc.nextLine();
+
+        while (true) {
+            if (reponse.equals("y")) {
+
+                System.out.println("quel est le pseudo de votre sauvegarde ?");
+
+                pseudo = sc.nextLine();
+                filePath = "src/sauvegarde/" + pseudo + ".txt";
+                try {
+                    ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filePath));
+                    dresseur = (Dresseur) ois.readObject();
+                    ois.close();
+                    System.out.println(dresseur.toString());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();   
+                }
+
+                break;
+
+            } else if (reponse.equals("n")) {
+                System.out.println("Entrez votre pseudo :");
+
+                pseudo = sc.nextLine();
+                
+                dresseur = new Dresseur(pseudo);
+
+                filePath = "src/sauvegarde/" + pseudo + ".txt";
+
+                try {
+                    // Création du flux de sortie vers le fichier
+                    FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+                    ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+                    // Enregistrement du nom du dresseur dans le fichier
+                    objectOutputStream.writeObject(dresseur);
+
+                    // Fermeture des flux
+                    objectOutputStream.close();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+
+            } else {
+                System.out.println("Vous n'avez pas choisi une option valide !");
+                System.out.println("Avez vous un compte ? (y/n)");
+                reponse = sc.nextLine();
+            }
+            
+        }
+
+        
+        
 
         System.out.println("Bonjour " + pseudo + "!");
 
@@ -30,23 +92,29 @@ public class Main {
             sc.nextLine(); // Pour consommer la nouvelle ligne après avoir lu l'entier
 
             switch (choix) {
+
                 case 1:
                     System.out.println("Vous avez choisi d'ouvrir une lootbox !");
                     Pokemon pokemon = PokemonGenerator.generateRandomPokemon();
                     System.out.println(pokemon.toString()); // Affiche les détails du Pokémon
 
                     System.out.println("Voulez-vous le garder ? (y/n)");
-                    String reponse = sc.nextLine();
-
-                    if (reponse.equals("y")) {
-                        dresseur.ajouterPokemon(pokemon);
-                        System.out.println("Pokémon ajouté à votre équipe !");
-                    } else if (reponse.equals("n")) {
-                        System.out.println("Vous avez choisi de ne pas garder ce Pokémon !");
-                    } else {
-                        System.out.println("Vous n'avez pas choisi une option valide !");
+                    reponse = sc.nextLine();
+                    while (true) {
+                        if (reponse.equals("y")) {
+                            dresseur.ajouterPokemon(pokemon);
+                            break;
+                        } else if (reponse.equals("n")) {
+                            System.out.println("Vous avez choisi de ne pas garder ce Pokémon !");
+                            break;
+                        } else {
+                            System.out.println("Vous n'avez pas choisi une option valide !");
+                            System.out.println("Voulez-vous le garder ? (y/n)");
+                            reponse = sc.nextLine();
+                        }
+                        
                     }
-                    break;
+                    
 
                 case 2:
                     if (dresseur.getPokemons().size() < 6) {
@@ -64,13 +132,44 @@ public class Main {
                     break;
 
                 case 4:
-                
+
                     System.out.println("Vous avez choisi de sauvegarder votre partie !");
+                    try {
+                        // Création du flux de sortie vers le fichier
+                        FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+                        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+                        // Enregistrement du nom du dresseur dans le fichier
+                        objectOutputStream.writeObject(dresseur);
+
+                        // Fermeture des flux
+                        objectOutputStream.close();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                     break;
-                    
 
                 case 5:
                     System.out.println("Vous avez choisi de charger une partie !");
+                    System.out.println("quel est votre pseudo ?");
+                    pseudo = sc.nextLine();
+                    filePath = "src/sauvegarde/" + pseudo + ".txt";
+                    try {
+                        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filePath));
+                        dresseur = (Dresseur) ois.readObject();
+                        ois.close();
+                        System.out.println(dresseur.toString());
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();   
+                    }
+
+
+
                     break;
 
                 case 6:
