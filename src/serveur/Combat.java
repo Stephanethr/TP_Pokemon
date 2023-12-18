edcaadba1ce85e92fc999f4c84eb4d70e359de81
Frédéric Turcq-Santucci <fturcq.santucci@gmail.com>
2023-12-18 14:23:01 +0100

    azef

--- src/serveur/Combat.java
+++ src/serveur/Combat.java
@@ -16,7 +16,46 @@ public class Combat {
 
     public void combat() {
 
-        
+        while (j1.getPokemonsEquipe().size() > 0 && j2.getPokemonsEquipe().size() > 0) {
+
+            System.out.println(j1.getPseudo() + " envoie " + j1.getPokemonsEquipe().get(0).getNom() + " ! \n");
+
+            System.out.println(j2.getPseudo() + " envoie " + j2.getPokemonsEquipe().get(0).getNom() + " ! \n");
+
+            j1.getPokemonsEquipe().get(0).attaque(j2.getPokemonsEquipe().get(0));
+
+            if(j2.getPokemonsEquipe().get(0).getEstVivant() == false){
+
+                System.out.println(j2.getPokemonsEquipe().get(0).getNom() + " agonise sur le sol  \n");
+                j2.getPokemonsEquipe().remove(0);
+
+            } 
+            else{
+                System.out.println(j2.getPokemonsEquipe().get(0).getPv() + " pv restant \n");
+            }
+
+            j2.getPokemons().get(0).attaque(j1.getPokemonsEquipe().get(0));
+
+            if(j1.getPokemonsEquipe().get(0).getEstVivant() == false){
+
+                System.out.println(j1.getPokemonsEquipe().get(0).getNom() + " agonise sur le sol  \n");
+                j1.getPokemonsEquipe().remove(0);
+
+            }
+            else{
+                System.out.println(j1.getPokemonsEquipe().get(0).getPv() + " pv restant \n");
+            }
+
+
+           
+        }
+
+        if (j1.getPokemonsEquipe().size() == 0) {
+            System.out.println(j2.getPseudo() + " a gagné !");
+        } else {
+            System.out.println(j1.getPseudo() + " a gagné !");
+        }
+
 
 
     }
