28f4743a7232f854887eb7971eb812dfd0154a90
Stephanethr <stethiry9@gmail.com>
2023-12-18 10:42:15 +0100

    add methode combat in pokemon

--- src/client/Pokemon.java
+++ src/client/Pokemon.java
@@ -105,11 +105,12 @@ public class Pokemon implements Serializable {
                 valeur = 10;
                 break;
 
-            // Si le Pokémon a 2 évolutions, il faut 5 bonbons pour la première et 10 pour la deuxième
+            // Si le Pokémon a 2 évolutions, il faut 5 bonbons pour la première et 10 pour
+            // la deuxième
             case 2:
                 valeur = 5;
                 break;
-            
+
             // Si le Pokémon a 3 évolutions (Evoli uniquement), il faut 10 bonbons
             case 3:
                 valeur = 10;
@@ -129,23 +130,22 @@ public class Pokemon implements Serializable {
             System.out.println("Votre Pokémon " + this.nom + " ne peut pas évoluer.");
             return;
         }
-    
+
         // Calculer le coût d'évolution
         int coutEvolution = this.verifCoutEvolution();
         System.out.println("Coût d'évolution pour " + this.nom + ": " + coutEvolution + " bonbons. \n");
-    
+
         // Vérifier si le dresseur a suffisamment de bonbons pour l'évolution
         if (dresseur.getNombreBonbonsParType(this.type) >= coutEvolution) {
             // Déduire les bonbons pour l'évolution
             dresseur.supprimerBonbon(this.type, coutEvolution);
-            
-    
+
             // Gérer l'évolution spécifique pour Evoli
             if (this.nom.equals("Evoli")) {
                 int randomEvolutionIndex = random.nextInt(this.evolutions.size());
                 this.nom = this.evolutions.get(randomEvolutionIndex);
                 this.evolutions.clear();
-    
+
                 // Mise à jour du type en fonction de l'évolution
                 switch (this.nom) {
                     case "Voltali":
@@ -165,7 +165,7 @@ public class Pokemon implements Serializable {
                 this.nom = this.evolutions.get(0);
                 this.evolutions.remove(0);
             }
-    
+
             // Mise à jour des stats du Pokémon après l'évolution
             this.pc += (int) (this.pc * 0.7); // Augmentation de 70% des PC
             this.pv += (int) (this.pv * 0.7); // Augmentation de 70% des PV
@@ -174,15 +174,350 @@ public class Pokemon implements Serializable {
             System.out.println("Vous n'avez pas assez de bonbons pour faire évoluer " + this.nom);
         }
     }
-    
 
-    public int calculerDegats() {
-        return (int) (this.pc * 0.1);
-    }
+    // Méthode pour faire combattre le Pokémon
+    public void combat(Pokemon pokemonAdverse) {
+        int degatsInitiaux = (int) (this.pc * 0.1);
 
-    // Méthode pour que le pokemon reçoive les dégâts
-    public void recevoirDegats(int degats) {
-        this.pv -= degats;
+        // Calcul des dégâts en fonction du type du Pokémon
+        // (Insecte, Glace, Normal, Spectre, Dragon, Electrique, Sol, Vol, Roche, Feu,
+        // Combat, Psy, Eau, Poison, Plante)
+        switch (this.type) {
+            case "Insecte":
+                switch (pokemonAdverse.type) {
+                    case "Insecte":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Combat":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Feu":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Plante":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Poison":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Psy":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Spectre":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Vol":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    default:
+                        break;
+                }
+                break;
+
+            case "Glace":
+                switch (pokemonAdverse.type) {
+                    case "Glace":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Dragon":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Eau":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Plante":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Feu":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Sol":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Vol":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case "Normal":
+                switch (pokemonAdverse.type) {
+                    case "Roche":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Spectre":
+                        pokemonAdverse.pv -= degatsInitiaux * 0;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case "Spectre":
+                switch (pokemonAdverse.type) {
+                    case "Normal":
+                        pokemonAdverse.pv -= degatsInitiaux * 0;
+                        break;
+                    case "Spectre":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Psy":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    default:
+                        break;
+                }
+            case "Dragon":
+                switch (pokemonAdverse.type) {
+                    case "Dragon":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    default:
+                        break;
+                }
+            case "Electrique":
+                switch (pokemonAdverse.type) {
+                    case "Eau":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Electrique":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Plante":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Vol":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Sol":
+                        pokemonAdverse.pv -= degatsInitiaux * 0;
+                        break;
+                    case "Dragon":
+                        pokemonAdverse.pv -= degatsInitiaux * 0.5;
+                        break;
+                    default:
+                        break;
+                }
+            case "Sol":
+                switch (pokemonAdverse.type) {
+                    case "Electrique":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Feu":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Insecte":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Plante":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Poison":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Roche":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Sol":
+                        pokemonAdverse.pv -= degatsInitiaux * 0;
+                        break;
+                    default:
+                        break;
+                }
+            case "Vol":
+                switch (pokemonAdverse.type) {
+                    case "Combat":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Electrique":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Plante":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Insecte":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Roche":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    default:
+                        break;
+                }
+            case "Roche":
+                switch (pokemonAdverse.type) {
+                    case "Combat":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Feu":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Insecte":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Glace":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Vol":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Sol":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    default:
+                        break;
+                }
+            case "Feu":
+                switch (pokemonAdverse.type) {
+                    case "Dragon":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Eau":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Feu":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Plante":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Glace":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Insecte":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Roche":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    default:
+                        break;
+                }
+            case "Combat":
+                switch (pokemonAdverse.type) {
+                    case "Insecte":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Roche":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Spectre":
+                        pokemonAdverse.pv -= degatsInitiaux * 0;
+                        break;
+                    case "Normal":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Glace":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Poison":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Vol":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Psy":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    default:
+                        break;
+                }
+            case "Psy":
+                switch (pokemonAdverse.type) {
+                    case "Combat":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Poison":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Psy":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    default:
+                        break;
+                }
+            case "Eau":
+                switch (pokemonAdverse.type) {
+                    case "Dragon":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Eau":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Plante":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Feu":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Sol":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Roche":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    default:
+                        break;
+                }
+            case "Poison":
+                switch (pokemonAdverse.type) {
+                    case "Insecte":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Plante":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Poison":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Sol":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Roche":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Spectre":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    default:
+                        break;
+                }
+            case "Plante":
+                switch (pokemonAdverse.type) {
+                    case "Eau":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Plante":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Poison":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Roche":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Sol":
+                        pokemonAdverse.pv -= degatsInitiaux * 2;
+                        break;
+                    case "Feu":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Insecte":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Dragon":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    case "Vol":
+                        pokemonAdverse.pv -= (int) (degatsInitiaux * 0.5);
+                        break;
+                    default:
+                        break;
+                }
+            default:
+                pokemonAdverse.pv -= degatsInitiaux;
+        }
+
     }
 
     // Méthode pour afficher les informations du Pokémon
